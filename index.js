54af6ff4af090cad9b213c41b9bd21b9b139ce5b
Júlia <juliaeeo@gmail.com>
2023-03-09 16:00:06 -0300

    inclusão do botão SaveTab

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 let myRecipe = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const tabBtn = document.getElementById("tab-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
 
 // Get the leads from the localStorage - PS: JSON.parse()
 const recipeFromLocalStorage = JSON.parse(localStorage.getItem("myRecipe"));
 
-// 1. Check if leadsFromLocalStorage is truthy
-// 2. If so, set myLeads to its value and call renderLeads()
+// Check if leadsFromLocalStorage is truthy
 if (recipeFromLocalStorage) {
   myRecipe = recipeFromLocalStorage;
-  renderRecipe();
+  render(myRecipe);
+}
+
+//Save Tab button
+tabBtn.addEventListener("click", function () {
+  // Grab the URL of the current tab!
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myRecipe.push(tabs[0].url);
+    localStorage.setItem("myRecipe", JSON.stringify(myRecipe));
+    render(myRecipe);
+  });
+})
+
+//puts the saved recipes into a list
+function render(recipes) {
+  let listItems = "";
+  for (let i = 0; i < recipes.length; i++) {
+    //listItems += "<li><a target='_blank' href='" + myRecipe[i] + "'>" + myRecipe[i] + "</a></li>";
+    listItems += `
+      <li>
+        <a target='_blank' href='${recipes[i]}'>
+          ${recipes[i]}
+        </a>
+      </li>
+    `
+  }
+  ulEl.innerHTML = listItems;
 }
 
 // Listen for double clicks on the delete button
@@ -19,7 +45,7 @@ if (recipeFromLocalStorage) {
 deleteBtn.addEventListener("dblclick", function () {
   localStorage.clear();
   myRecipe = [];
-  renderRecipe();
+  render(myRecipe);
 })
 
 //Input button, adds event when clicked
@@ -28,21 +54,6 @@ inputBtn.addEventListener("click", function () {
   inputEl.value = "";
   //save the myRecipe array to localStorage
   localStorage.setItem("myRecipe", JSON.stringify(myRecipe));
-  renderRecipe();
+  render(myRecipe);
 })
 
-//puts the saved recipes into a list
-function renderRecipe() {
-  let listItems = "";
-  for (let i = 0; i < myRecipe.length; i++) {
-    //listItems += "<li><a target='_blank' href='" + myRecipe[i] + "'>" + myRecipe[i] + "</a></li>";
-    listItems += `
-      <li>
-        <a target='_blank' href='${myRecipe[i]}'>
-          ${myRecipe[i]}
-        </a>
-      </li>
-    `
-  }
-  ulEl.innerHTML = listItems;
-}
